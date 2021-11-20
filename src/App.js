90042e3420605c13001cde5df795ebc2a6bb17ba
Jeff <jolson615@gmail.com>
2021-11-20 10:13:41 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import DataView from './components/DataView.js';
-import Navbar from './components/Navbar.js';
-import './App.css';
+import React from "react";
+import DataView from "./components/DataView.js";
+import Navbar from "./components/Navbar.js";
+import "./App.css";
+
+// leaving a comment hello!
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <h1 className="header">Earn More, Pay Less? Average Housing Costs and Incomes by NYC Borough</h1>
+      <Navbar />
+      <h1 className="header">
+        Earn More, Pay Less? Average Housing Costs and Incomes by NYC Borough
+      </h1>
 
-      <DataView/>
+      <DataView
+        borough="Staten Island"
+        renterCost="$14,292"
+        ownerCost="$29,752"
+      />
     </div>
   );
 }
